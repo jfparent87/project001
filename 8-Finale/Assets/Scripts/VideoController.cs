63f1b416ffdc2c51fad7da07f32891a0db03a0d5
GEL\jfpar59 <gausshiggs@gmail.com>
2017-01-17 12:46:10 -0500

    added videocontroller

---
+++ 8-Finale/Assets/Scripts/VideoController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class VideoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        GetComponent<AudioSource>().Play();
+        movie.Play();
+	}
+	
+}
